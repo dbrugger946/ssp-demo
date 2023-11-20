c0a8c8eec3ccb2e8abf36341b6be959e84ca0c99
chaika <sea5@bird888.com>
2023-11-20 13:10:37 -0500

    updates to configurations

--- camel-edge/src/main/java/com/edgedemo/ResultsProcessor.java
+++ camel-edge/src/main/java/com/edgedemo/ResultsProcessor.java
@@ -1,8 +1,10 @@
 package com.edgedemo;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
+@RegisterForReflection
 public class ResultsProcessor implements Processor {
     @Override
     public void process(Exchange exchange) throws Exception {
